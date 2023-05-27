24732373e46ccb377de649e9a507045a39724dfd
Andrii <sushkoandy1@gmail.com>
2023-05-27 16:43:44 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-blended-3">
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyles} />
         <App />
